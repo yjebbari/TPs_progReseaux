9bcb91916d557bb085444025da5e7d835880ac59
yjebbari <yousra.jebbari@insa-lyon.fr>
2021-11-24 11:41:17 +0100

    content 4

--- ChatAppRetry/src/module-info.java
+++ ChatAppRetry/src/module-info.java
@@ -1,9 +1,7 @@
 module chatApplication {
+	exports UDP;
+	exports stream;
+	exports View;
+
 	requires java.desktop;
-	/*
-	 * The application has been developed using a tutorial from WittCode (youtube channel : https://www.youtube.com/channel/UCJnCCO9QVAP9saemgZ9tpoQ)
-	 * video link : https://youtu.be/gLfuZrrfKes
-	 * upload date : August 17th 2021
-	 * watch date November 21st 2021
-	 */
 }
